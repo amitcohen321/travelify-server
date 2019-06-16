53f226efcdeae02215f2d657b07c0cc8f437f98a
Amit Cohen <amit_cohen321@hotmail.com>
2019-06-16 10:56:32 +0300

    Bugs Fixes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express()
 
 // MIDDLEWARE
 app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*")
+	res.setHeader("Access-Control-Allow-Origin", "https://travelify.amitco.info")
 	res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization")
 	next()
