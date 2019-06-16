087f99806cbcdaf6bc850bb791ef9772936f6ef4
Amit Cohen <amit_cohen321@hotmail.com>
2019-06-16 17:21:10 +0300

    Delete consts.js

--- consts.js
+++
@@ -1,8 +0,0 @@
-// CONSTS & APIs
-const SENDGRID_API_KEY = "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"
-const MONGO_DB_CONNECT_STRING =
-	"mongodb+srv://amitco:fSn5mMS4PB9Rzzoh@travelify-cluster-ezgb2.mongodb.net/travelify?retryWrites=true"
-
-// export
-exports.SENDGRID_API_KEY = SENDGRID_API_KEY
-exports.MONGO_DB_CONNECT_STRING = MONGO_DB_CONNECT_STRING
